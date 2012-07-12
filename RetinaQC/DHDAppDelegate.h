5d9e65ef49fa93b1f4f40c95d7b6c98836f5a4bb
Douglas Heriot <douglas@douglasheriot.com>
2012-07-12 23:25:06 +1000

    Initial Commit

---
+++ RetinaQC/DHDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DHDAppDelegate.h
+//  RetinaQC
+//
+//  Created by Douglas Heriot on 12/07/12.
+//  Copyright (c) 2012 Douglas Heriot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DHDAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ RetinaQC/DHDAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  DHDAppDelegate.m
+//  RetinaQC
+//
+//  Created by Douglas Heriot on 12/07/12.
+//  Copyright (c) 2012 Douglas Heriot. All rights reserved.
+//
+
+#import "DHDAppDelegate.h"
+
+@implementation DHDAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
